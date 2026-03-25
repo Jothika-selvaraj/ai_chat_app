b464b69d5eb44dbdacb8700563c88dffd0d32ecd
Jothika-selvaraj <jothikaselvaraj8787@gmail.com>
2026-03-25 16:34:58 +0530

    get api

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ app.use(express.json());
 connectDB();
 
 app.use("/api", aiRoutes);
-
+app.get("/", (req, res) => {
+  res.send("API is running ");
+});
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
